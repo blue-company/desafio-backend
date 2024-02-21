0f11b08433f3ff0b0b8fce73905a7066aa3d1793
abnerferreiradesousa <abnerferreira84@gmail.com>
2024-02-21 20:30:54 -0300

    [refactor]: Changing code to user Route Annotation from Symfony and functionality still working.

--- blue_appointment_api/src/Controller/UserController.php
+++ blue_appointment_api/src/Controller/UserController.php
@@ -1,26 +1,37 @@
 <?php
- 
+
 namespace App\Controller;
- 
+
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Persistence\ManagerRegistry;
-use App\Entity\User;
- 
-#[Route('/api', name: 'api_')]
+
+/**
+ * @Route("/api", name="api_")
+ */
 class UserController extends AbstractController
 {
-    #[Route('/register', name: 'register', methods: 'post')]
-    public function index(ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    private ManagerRegistry $managerRegistry;
+
+    public function __construct(ManagerRegistry $managerRegistry)
     {
-        $em = $doctrine->getManager();
+        $this->managerRegistry = $managerRegistry;
+    }
+
+    /**
+     * @Route("/register", name="register", methods={"POST"})
+     */
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    {
+        $em = $this->managerRegistry->getManager();
         $decoded = json_decode($request->getContent());
         $email = $decoded->email;
         $plaintextPassword = $decoded->password;
-   
+
         $user = new User();
         $hashedPassword = $passwordHasher->hashPassword(
             $user,
@@ -31,7 +42,7 @@ class UserController extends AbstractController
         $user->setUsername($email);
         $em->persist($user);
         $em->flush();
-   
+
         return $this->json(['message' => 'Registered Successfully']);
     }
 }
