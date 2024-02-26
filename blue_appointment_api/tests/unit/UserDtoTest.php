08d814e7fe52b4d266438c02dcc039ae745d8dbe
abnerferreiradesousa <abnerferreira84@gmail.com>
2024-02-26 11:44:38 -0300

    [test]: Add at least one unit test.

---
+++ blue_appointment_api/tests/unit/UserDtoTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Test\Infra\Dto;
+
+use App\Infra\Dto\UserDto;
+use PHPUnit\Framework\TestCase;
+
+class UserDtoTest extends TestCase
+{
+    public function testConstructWithLasagnaData()
+    {
+        $requestData = [
+            'id' => 1,
+            'email' => 'garfield@lasagnalovers.com',
+            'sex' => 'male',
+            'fullName' => 'Garfield Cat',
+            'birthDate' => '1978-06-19',
+            'username' => 'garfield',
+            'roles' => ['ROLE_CAT'],
+            'password' => 'ilove lasagna',
+        ];
+
+        $userDto = new UserDto($requestData);
+
+        $this->assertEquals($requestData['id'], $userDto->id);
+        $this->assertEquals($requestData['email'], $userDto->email);
+        $this->assertEquals($requestData['sex'], $userDto->sex);
+        $this->assertEquals($requestData['fullName'], $userDto->fullName);
+        $this->assertEquals(new \DateTime($requestData['birthDate']), $userDto->birthDate);
+        $this->assertEquals($requestData['email'], $userDto->username);
+        $this->assertEquals($requestData['roles'], $userDto->roles);
+        $this->assertEquals($requestData['password'], $userDto->password);
+    }
+
+    public function testConstructWithEmptyBowlData()
+    {
+        $requestData = [];
+
+        $userDto = new UserDto($requestData);
+
+        $this->assertNull($userDto->id);
+        $this->assertNull($userDto->email);
+        $this->assertNull($userDto->sex);
+        $this->assertNull($userDto->fullName);
+        $this->assertNull($userDto->birthDate);
+        $this->assertNull($userDto->username);
+        $this->assertEquals([], $userDto->roles);
+        $this->assertNull($userDto->password);
+    }
+}
