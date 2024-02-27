edc2ec11165e7734f75917a028210f1151b6e581
abnerferreiradesousa <abnerferreira84@gmail.com>
2024-02-27 20:50:38 -0300

    [config]: Adjust docker-compose, Dockerfile adding bcmath dependency and allowing Authorization Header on Apache config.

--- blue_appointment_api/config/bundles.php
+++ blue_appointment_api/config/bundles.php
@@ -12,4 +12,6 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
--- blue_appointment_api/docker/entrypoint.sh
+++ blue_appointment_api/docker/entrypoint.sh
@@ -3,6 +3,11 @@
 # Install Composer dependencies
 composer install -n
 
+# Wait for 10 seconds
+sleep 60
+
+bin/console doctrine:database:create --if-not-exists --no-interaction
+
 # Run Doctrine migrations
 bin/console doctrine:migrations:migrate --no-interaction
 
