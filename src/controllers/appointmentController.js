33d47a1344bafc3502cbfba535c7f25313be6fb4
Rafael Santos <rafaelcs_94@hotmail.com>
2024-05-01 20:53:28 -0300

    feat: create appointmentController

---
+++ src/controllers/appointmentController.js
@@ -0,0 +1,52 @@
+const appointmentService = require('../services/appointmentService');
+const { CREATED, OK_STATUS } = require('../utils/statusCode');
+const stream = require('stream');
+
+const create = async (req, res) => {
+  const { userId } = req.user;
+  const token = await appointmentService.create(req.body, userId);
+
+  const urlLink = `http://localhost:3000/appointments/${token}`;
+
+  res.status(CREATED).json({ urlLink });
+};
+
+const getAppointment = async (req, res) => {
+  const { token } = req.params;
+
+  const { pdfFile, appointmentId } = await appointmentService.getAppointment(token);
+  const fileContents = Buffer.from(pdfFile, 'base64');
+
+  const readStream = new stream.PassThrough();
+  readStream.end(fileContents);
+
+  res.setHeader('Content-Disposition', `attachment; filename=consulta_${appointmentId}.pdf`);
+  res.setHeader('Content-Type', 'application/pdf');
+
+  readStream.pipe(res);
+};
+
+const update = async (req, res) => {
+  const { id: appointmentId } = req.params;
+  const { userId } = req.user;
+  const token = await appointmentService.update(appointmentId, req.body, userId);
+
+  const urlLink = `http://localhost:3000/appointments/${token}`;
+
+  res.status(OK_STATUS).json({ urlLink });
+};
+
+const cancel = async (req, res) => {
+  const { id: appointmentId } = req.params;
+  const { userId } = req.user;
+  await appointmentService.cancel(appointmentId, userId);
+
+  res.status(OK_STATUS).json({ message: 'Appointment canceled' });
+};
+
+module.exports = {
+  create,
+  getAppointment,
+  update,
+  cancel,
+};
