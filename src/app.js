f910dac1962f5a1d575cbaf903840ae4d5983c27
Rafael Santos <rafaelcs_94@hotmail.com>
2024-05-01 20:54:20 -0300

    feat: update app and server files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 require('express-async-errors');
 const express = require('express');
-const cors = require('cors');
+const userRouter = require('./routes/userRouter');
+const appointmentRouter = require('./routes/appointmentRouter');
+const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use('/users', userRouter);
+
+app.use('/appointments', appointmentRouter);
+
+app.use(errorMiddleware);
 
 module.exports = app;
