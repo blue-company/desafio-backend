e7420384370fe032ba83fc5f7dd1f1c5258ca462
Rafael Santos <rafaelcs_94@hotmail.com>
2024-05-03 12:40:41 -0300

    feat: add swagger documentation

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const userRouter = require('./routes/userRouter');
 const appointmentRouter = require('./routes/appointmentRouter');
 const errorMiddleware = require('./middlewares/error');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('../swagger_output.json');
 
 const app = express();
 app.use(express.json());
@@ -12,5 +14,7 @@ app.use('/users', userRouter);
 app.use('/appointments', appointmentRouter);
 
 app.use(errorMiddleware);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 module.exports = app;
