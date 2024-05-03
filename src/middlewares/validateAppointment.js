438fc653edc79483e5054d786a66c5d798d738c6
Rafael Santos <rafaelcs_94@hotmail.com>
2024-05-03 12:13:03 -0300

    ref: update joi error message on validateAppointment

--- src/middlewares/validateAppointment.js
+++ src/middlewares/validateAppointment.js
@@ -5,10 +5,16 @@ const schema = Joi.object({
   reason: Joi.string().required(),
   appointmentDate: Joi.string()
     .regex(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/)
-    .required(),
+    .required()
+    .messages({
+      'string.pattern.base': '"appointmentDate" must be of format YYYY-MM-DD',
+    }),
   appointmentTime: Joi.string()
     .regex(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/)
-    .required(),
+    .required()
+    .messages({
+      'string.pattern.base': '"appointmentTime" must be of format HH:MM',
+    }),
 });
 
 module.exports = (req, _res, next) => {
