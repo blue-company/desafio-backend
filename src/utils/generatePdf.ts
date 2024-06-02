427612a005d0aee4d215dff7d18ce19c8d759e28
gsousaaa <glaucoezequiel94@gmail.com>
2024-06-02 16:43:51 -0300

    Alterando o pdfGenerator e a renderização do html

--- src/utils/generatePdf.ts
+++ src/utils/generatePdf.ts
@@ -1,29 +1,35 @@
 import path from "path";
-import ejs from 'ejs';
 import pdf from 'html-pdf'
+import { renderHtml } from "./renderHtml";
 
+interface DataPdf {
+    user_id: number | undefined,
+    username: string,
+    doctorName: string,
+    consultationTime: string,
+    consultationDate: string,
+    speciality: string
+}
 
-export const generatePDF= async (user_id: number | undefined, username: string, doctorName: string, consultationTime: string, consultationDate: string, speciality: string): Promise <string | Error>  => {
+export const generatePDF = async (data: DataPdf): Promise<string | Error> => {
     try {
+        let { username, doctorName, consultationTime, consultationDate, speciality } = data
         let randomNumber = Math.floor(Math.random() * 1000000)
-         ejs.renderFile(path.join(__dirname,'../views/pdf.ejs'), {username, doctorName, consultationTime, consultationDate, speciality},  (err, html) => {
-            if(err) {
-                 return new Error('Error rendering EJS');
+        const html = await renderHtml({ username, doctorName, consultationTime, consultationDate, speciality })
+
+        pdf.create(html, {}).toFile(path.join(__dirname, `../views/consultations/${username.replace(/\s+/g, '')}_${data.user_id}/${username.replace(/\s+/g, '')}_${doctorName.replace(/\s+/g, '')}_consulta_${randomNumber}.pdf`), (err, res) => {
+            if (err) {
+                return new Error(err.message)
             } else {
-                console.log(html)
-                pdf.create(html, {}).toFile(path.join(__dirname, `../views/consultations/${username.replace(/\s+/g, '')}_${user_id}/${username.replace(/\s+/g, '')}_${doctorName.replace(/\s+/g, '')}_consulta_${randomNumber}.pdf`), (err, res) => {
-                    if(err) {
-                        return new Error(err.message)
-                    } else {
-                        console.log(res)
-                    }
-                })
-            }})
+                console.log(res)
+            }
+        })
 
-            let file = `/consultations/${username.replace(/\s+/g, '')}_${user_id}/${username.replace(/\s+/g, '')}_${doctorName.replace(/\s+/g, '')}_consulta_${randomNumber}.pdf`
-            return file
+        let file = `/consultations/${data.username.replace(/\s+/g, '')}_${data.user_id}/${data.username.replace(/\s+/g, '')}_${data.doctorName.replace(/\s+/g, '')}_consulta_${randomNumber}.pdf`
+        return file
 
-    } catch(err: any) {
-        return new Error(err.message)
+    } catch (err: any) {
+        throw new Error(err.message)
     }
+
 }
