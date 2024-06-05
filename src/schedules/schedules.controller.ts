e6643e691749a468fc5129296461e17a1d7339e6
Willian Alves Batista <willian.alves.b15@gmail.com>
2024-06-05 16:20:48 -0300

    feat: create schedules and pdf

--- src/schedules/schedules.controller.ts
+++ src/schedules/schedules.controller.ts
@@ -1,26 +1,71 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
+import * as puppeteer from 'puppeteer';
+import * as ejs from 'ejs';
+import * as path from 'path';
 import { SchedulesService } from './schedules.service';
 import { CreateSchedulesDto } from './dto/create-schedules.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { UserFromJwt } from 'src/auth/models/UserFromJwt';
+import { UserService } from 'src/user/user.service';
+import { formatCPF, toConvertHours, toDateUtc } from 'src/utils';
 
 @Controller('schedules')
 export class SchedulesController {
-  constructor(private readonly schedulesService: SchedulesService) {}
+  constructor(
+    private readonly schedulesService: SchedulesService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post()
   @UseGuards(JwtAuthGuard)
   async create(
     @Body() createSchedulesDto: CreateSchedulesDto,
-    @CurrentUser() user: UserFromJwt,
+    @CurrentUser() userToken: UserFromJwt,
+    @Res() res: Response,
   ) {
     const dateTimeString = `${createSchedulesDto.date}T${createSchedulesDto.hours}`;
-    // console.log(teste.toLocaleString(), '-----------toLocaleDateString');
-    return this.schedulesService.create({
+    const schedules = await this.schedulesService.create({
       dateTime: new Date(dateTimeString),
-      user_id: user.id,
+      user_id: userToken.id,
       medicalSpecialty: createSchedulesDto.medicalSpecialty,
     });
+
+    const user = await this.userService.findByEmail(userToken.email);
+
+    const schedulesUserData = {
+      cpf: formatCPF(user.cpf),
+      name: user.name.toUpperCase(),
+      medicalSpecialty: schedules.medicalSpecialty.toUpperCase(),
+      dateBirth: toDateUtc(user.dateBirth),
+      date: schedules.dateTime.toLocaleDateString(),
+      hours: toConvertHours(schedules.dateTime),
+    };
+
+    const html = await ejs.renderFile(
+      path.resolve(__dirname, '../../src/assets/schedules-pdf.ejs'),
+      { schedulesUserData },
+    );
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setContent(html, { waitUntil: 'networkidle0' });
+
+    const pdfBuffer = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+    });
+
+    await browser.close();
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader(
+      'Content-Disposition',
+      'attachment; filename=user_profile.pdf',
+    );
+
+    return res.send(pdfBuffer);
   }
 }
--- src/schedules/schedules.module.ts
+++ src/schedules/schedules.module.ts
@@ -4,15 +4,23 @@ import { SchedulesController } from './schedules.controller';
 import { SchedulesRepository } from 'src/repositories/schedules-repository';
 import { PrismaSchedulesRepository } from 'src/repositories/prisma/prisma-schedules-repository';
 import { PrismaService } from 'src/database/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { UserRepository } from 'src/repositories/user-repository';
+import { PrismaUserRepository } from 'src/repositories/prisma/prisma-user-repository';
 
 @Module({
   controllers: [SchedulesController],
   providers: [
     SchedulesService,
+    UserService,
     PrismaService,
     {
       provide: SchedulesRepository,
       useClass: PrismaSchedulesRepository,
+    },
+    {
+      provide: UserRepository,
+      useClass: PrismaUserRepository,
     },
   ],
   exports: [SchedulesService],
